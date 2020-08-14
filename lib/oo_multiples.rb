cb8a0db0c8c0700be523b684ab53eb28f8782ebb
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-14 18:58:27 +0000

    Automatically backed up by Learn

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -8,4 +8,13 @@ class Multiples
     @multiples = collect_multiples
   end
 
+  def collect_multiples
+    (1...limit).to_a.select{|n| n % 3 == 0 || n % 5 == 0}
+  end
+
+  def sum_multiples
+    @multiples.reduce(0, :+)
+  end
+
+    
 end
