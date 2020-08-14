10dba436c9ceef551d21dab478ee4bd63f7ad651
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-14 18:57:36 +0000

    Automatically backed up by Learn

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,9 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+  attr_reader :limit, :multiples
+
+  def initialize(limit)
+    @limit = limit
+    @multiples = collect_multiples
+  end
