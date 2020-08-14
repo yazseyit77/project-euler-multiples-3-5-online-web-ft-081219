2c74a5cc2761b0a0a0183ea7353df47ecde889f3
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-14 18:58:45 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,9 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+  (1...limit).to_a.select{|n| n % 3 == 0 || n % 5 == 0}
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).reduce(0, :+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,20 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+  attr_reader :limit, :multiples
+
+  def initialize(limit)
+    @limit = limit
+    @multiples = collect_multiples
+  end
+
+  def collect_multiples
+    (1...@limit).to_a.select{|n| n % 3 == 0 || n % 5 == 0}
+  end
+
+  def sum_multiples
+    @multiples.reduce(0, :+)
+  end
+
+
+end
